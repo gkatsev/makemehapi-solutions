0d68652eef5d2ffa62e0ce773c15f7c1b635e6fc
Gary Katsevman <git@gkatsev.com>
2015-02-08 00:56:32 -0500

    10 validation with joi object

---
+++ 10_validation_object.js
@@ -0,0 +1,34 @@
+var Hapi = require('hapi');
+var Joi = require('joi');
+var server = new Hapi.Server();
+
+server.connection({
+  host: 'localhost',
+  port: Number(process.argv[2]) || 8080
+});
+
+server.route({
+  path: '/login',
+  method: 'POST',
+  handler: function(req, reply) {
+    reply("login successful");
+  },
+  config: {
+    validate: {
+      payload: Joi.object({
+        isGuest: Joi.boolean().required(),
+        username: Joi.string()
+          .when('isGuest', {
+            is: false,
+            then: Joi.required()
+          }),
+        password: Joi.string().alphanum(),
+        accessToken: Joi.string().alphanum()
+      })
+      .options({allowUknown: true})
+      .without('password', 'accessToken')
+    }
+  }
+});
+
+server.start();
